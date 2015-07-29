02c57376ff8bbe0247dae0e4a7b91df8991eed7f
Anton von Weltzien <anton.weltzien@gmail.com>
2015-07-29 23:18:44 +0200

    umlaut

--- core/src/de/rami/polygonViewer/Settings.java
+++ core/src/de/rami/polygonViewer/Settings.java
@@ -7,7 +7,7 @@ import de.rami.polygonViewer.Line;
 
 public class Settings {
 	
-	//Zur Optimierung der Struktur wird f�r den Mittelwert eine Eigene Klasse festgelegt.
+	//Zur Optimierung der Struktur wird fuer den Mittelwert eine Eigene Klasse festgelegt.
 	public static final float middle = 10.34f;
 	
 	public static int obererSchwellenWert = 200;
